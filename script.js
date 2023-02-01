5bac3098fd26b10508fe5d47f1db997e5214061d
Ian de Jesus <ian.deejay.iv@gmail.com>
2023-02-01 17:04:57 +0800

    feat: add new canvas button

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-const SKETCH_AREA_DIMENSIONS = 16 * 16;
+const SKETCH_AREA_DIMENSIONS = 16 * 16; // dimensions of the drawing space window, tightly coupled with .window--sketch-area dimensions, not responsive
+let selectedColor = 'red';
 
 const sketchArea = document.querySelector('.window--sketch-area');
 
@@ -7,5 +8,9 @@ for (let i = 0; i < SKETCH_AREA_DIMENSIONS; i++)
 {
     const newTile = document.createElement('div');
     newTile.setAttribute('class', 'window--sketch-area__tile');
+    newTile.addEventListener('mouseover', (e) => {
+        e.target.style.backgroundColor = selectedColor;
+    });
+
     sketchArea.appendChild(newTile);
 }
