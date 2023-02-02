481627c840beb5ca925a2087ab08dee9a4654ff4
Luzefiru <ian.deejay.iv@gmail.com>
2023-02-02 10:13:34 +0800

    feat: add rainbow pen toggle button

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 const SKETCH_AREA_DIMENSIONS = 16 * 16; // dimensions of the drawing space window, tightly coupled with .window--sketch-area dimensions, not responsive
-let selectedColor = 'red';
+let selectedColor = 'black';
 
 const sketchArea = document.querySelector('.window--sketch-area');
-const newGridButton = document.querySelector('.button--new-grid');
+const newGridButton = document.querySelector('.tile-menu__button--new-grid');
 newGridButton.addEventListener('click', clearCanvas);
+const colorPickerButton = document.querySelector('.tile-menu__button--color-picker');
+colorPickerButton.addEventListener('click', colorPickerUI);
 
 /* initialize the canvas grid tiles */
 for (let i = 0; i < SKETCH_AREA_DIMENSIONS; i++)
@@ -23,6 +25,11 @@ for (let i = 0; i < SKETCH_AREA_DIMENSIONS; i++)
 function clearCanvas () {
     const tilesList = document.querySelectorAll('.window--sketch-area__tile');
     tilesList.forEach((e) => e.style.backgroundColor = ''); // reset the backgroundColor to CSS specifications
+}
+
+function colorPickerUI () {
+    let inputtedColor = prompt('What color do you want to paint with?', 'black');
+    changePenColor(inputtedColor);
 }
 
 /**
